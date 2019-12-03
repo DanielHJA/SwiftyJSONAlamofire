6c4f4befed35b7d75c9574db5b2d17fd628f91c7
Daniel <daniel.hjartstrom@gmail.com>
2019-12-03 10:12:56 +0100

    Initial Commit

---
+++ AlamofireAndSwiftyJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireAndSwiftyJSON
+//
+//  Created by Daniel Hjärtström on 2019-12-03.
+//  Copyright © 2019 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
